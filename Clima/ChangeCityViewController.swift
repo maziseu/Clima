4a2ff60897f641dd773bf2b4da96dbfdddd8fe21
maziseu <maziseu@gmail.com>
2017-11-28 13:35:31 -0800

    Upload finished project

---
+++ Clima/ChangeCityViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ChangeCityViewController.swift
+//  WeatherApp
+//
+//  Created by mazi on 27/11/2017.
+//  Copyright (c) 2017. All rights reserved.
+//
+
+import UIKit
+
+
+//Write the protocol declaration here:
+protocol ChangeCityDelegate {
+    func userEnteredANewCityName(city: String)
+}
+
+
+class ChangeCityViewController: UIViewController {
+    
+    //Declare the delegate variable here:
+    var delegate : ChangeCityDelegate?
+    
+    //This is the pre-linked IBOutlets to the text field:
+    @IBOutlet weak var changeCityTextField: UITextField!
+
+    
+    //This is the IBAction that gets called when the user taps on the "Get Weather" button:
+    @IBAction func getWeatherPressed(_ sender: AnyObject) {
+        
+        let cityName = changeCityTextField.text!
+        
+        delegate?.userEnteredANewCityName(city: cityName)
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+
+    //This is the IBAction that gets called when the user taps the back button. It dismisses the ChangeCityViewController.
+    @IBAction func backButtonPressed(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
